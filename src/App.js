d74b93ee9c970b50f8c752a569dcbd792c2ba9bd
Liversticks <55716846+Liversticks@users.noreply.github.com>
2021-05-01 14:11:49 -0700

    support basic currencies

--- src/App.js
+++ src/App.js
@@ -2,55 +2,77 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react";
 import Uwuifier from "uwuifier";
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+import getSymbolFromCurrency from 'currency-symbol-map';
+
+const currencies = [
+  "USD",
+  "EUR",
+  "GBP",
+  "JPY"
+]
+
+const APIKEY = "b0e918fb6aba705171ea148727f821b9";
 
 // Get data from API
 // Render
+// Assume all prices come in as CAD
 function App() {
   const [data, setData] = useState([]);
+  const [currency, setCurrency] = useState("");
+  const [rate, setRate] = useState(1);
+  
   useEffect(() => {
     async function fetchData() {
       const result = await fetch("https://fakestoreapi.com/products");
       const jsonRes = await result.json();
-      await console.log(jsonRes);
+      console.log(jsonRes);
       setData(jsonRes);
     }
 
     fetchData();
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    async function fetchCurrency() {
+      setCurrency(currencies[Math.floor(Math.random() * currencies.length)]);
+      const url = `http://api.exchangeratesapi.io/v1/latest?access_key=${APIKEY}&symbols=CAD,${currency}`;
+      const rateObj = await fetch(url);
+      const rateRes = await rateObj.json();
+      console.log(rateRes)
+      setRate(rateRes.rates[currency]/rateRes.rates["CAD"]);
+      
+    }
+    fetchCurrency();
+    
+  }, [currency]);
+
+
   const uwuifier = new Uwuifier();
+  const symbol = getSymbolFromCurrency(currency);
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
+    <div>
+      <div className="of-container">
         {data.map(item => (
-          <div key={item.id} className="box">
-              <div className="itemHeader">
-                <div>{item.title}</div>
-                <div>{item.price}</div>
-              </div>
-              <div className="description">
-                {uwuifier.uwuifySentence(item.description)}
-              </div>
-              <img 
+          <div key={item.id}>
+            <Card className="box">
+              <CardBody>
+                <CardTitle tag="h2">{uwuifier.uwuifySentence(item.title)}</CardTitle>
+                <CardSubtitle tag="h3" className="text-muted">{symbol} {Number.parseFloat(item.price * rate).toFixed(2)}</CardSubtitle>
+              </CardBody>
+              <CardImg 
                 src={item.image}
                 alt={item.title + "bruh moment"}
-                className="productImg"  
-              ></img>
-              
+                top width="100%"  
+              />
+              { /*
+              <CardBody>
+                <CardText>{uwuifier.uwuifySentence(item.description)}</CardText>
+              </CardBody>
+              */ }
+            </Card>             
           </div>
         ))}
       </div>
