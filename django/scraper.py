2e3aaceebf6079364c5db5cba799631f9602a213
bsaw <brendan_saw@sfu.ca>
2021-05-01 14:46:12 -0700

    Added some scraper text

---
+++ django/scraper.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.google.com/search?q=phone&tbm=shop'
+
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+productnames = soup.find_all("div", attrs={"class": "rgHvZc"})
+productnamesList = []
+
+for i in productnames:
+    j = i.find('a')
+    #print(j.text)
+    productnamesList.append(j.text)
+    #print('\n')
+
+productImagesList = []
+productImages = soup.find_all("div", attrs={"class": "eUQRje"})
+for i in productImages:
+    j = i.find('img')
+    #print(j['src'])
+    productImagesList.append(j['src'])
+
+#print(productnamesList)
+#print(productImagesList)
+
+
+#print(f'number of productnames: {len(productnamesList)}, number of productimages: {len(productImagesList)}')
+output = []
+for i in range(0, len(productnames)):
+    output.append([productnamesList[i], productImagesList[i]])
+print(output)
+#print(realproductnames)
+'''children = productnames.findChildren("a" , recursive=False)
+for child in children:
+    print(child)'''
